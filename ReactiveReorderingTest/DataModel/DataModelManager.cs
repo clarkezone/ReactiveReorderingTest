7cad20ac412d9f4b772e11cb5e3721747d980bc0
James Clarke <james@clarkezone.net>
2017-08-23 05:36:49 -0700

    missing file

---
+++ ReactiveReorderingTest/DataModel/DataModelManager.cs
@@ -0,0 +1,14 @@
+﻿using Realms;
+
+namespace ReactiveReorderingTest.DataModel
+{
+    static class DataModelManager
+    {
+        const string REALMDBNAME = "testdb";
+
+        public static Realm RealmInstance { get {
+                return Realm.GetInstance(REALMDBNAME);
+            }
+        }
+    }
+}
---
+++ ReactiveReorderingTest/ViewModels/EpisodeViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ReactiveReorderingTest.DataModel;
+using Realms;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Input;
+
+namespace ReactiveReorderingTest.ViewModels
+{
+    class EpisodeViewModel : ViewModelBase
+    {
+        private RelayCommand _incrementCommand;
+
+        public DataModel.Episode Episode { get; set; }
+
+        public ICommand AddUpNext { get; set; }
+
+        public RelayCommand AddItemUpNextCommand
+        {
+            get
+            {
+                return _incrementCommand
+                    ?? (_incrementCommand = new RelayCommand(
+                    () =>
+                    {
+                        var upNextQueue = DataModelManager.RealmInstance.All<UpNextQueue>().FirstOrDefault();
+                        
+
+                        // UpNextQueueEntry en = new UpNextQueueEntry() { Episode = e, QuePosition = 0 };
+                        //u.Queue.Insert(0, en);
+                    }));
+            }
+        }
+    }
+}
